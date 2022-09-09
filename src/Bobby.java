9b21a647681865b330bfb4ac3be3f5cb27738c79
Srao2020 <contact@surfsyde.com>
2022-09-09 11:56:04 -0400

    hi

---
+++ src/Bobby.java
@@ -0,0 +1,48 @@
+public class Bobby {
+    // encapsulation
+    private String name;
+    private int age; // primative data: holdover fromC
+    private double coolnessFactor; // camelCase
+    /*
+    primatives from C/C++
+    int (short, int, long), double (float), boolean, char
+     */
+    private final double PI = 3.141578;
+
+    /**
+     * Javadoc comment
+     * Constructor -- build an object of this class
+     */
+    /**
+     * Parameterize constructor for class Bobby
+     * @param n name
+     * @param a age
+     * @param c coolnessFactor
+     */
+    public Bobby(String n, int a, double c) {
+        name = n;       // ASSIGNMENT OPERATOR -- right-assosiative
+        age = a;
+        coolnessFactor = c;
+    }
+
+    // ACCESSOR
+    public String getName()     {   return name;    }
+    public int getAge()         {   return age;    }
+
+    /**
+     * public return of coolnessFactor
+     * @return a double representing how cool this Bobby is
+     */
+    public double getCoolness() {   return coolnessFactor;  }
+
+    public String toString()    {
+        return name + " is " + age + " years old and is " +
+                (coolnessFactor*100) + "% cool";
+    }
+
+    public static void main(String[] args) {
+        Bobby b = new Bobby("Marge", 5000, .53);
+        System.out.println(b.getAge());
+    }
+}
+
